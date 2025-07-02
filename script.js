9e93cedd05321a10e0dde6fd15442bb9385c3856
Ravi Agrahari <agpashupati@gmail.com>
2025-07-02 23:06:41 +0545

    added code

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Wait for the DOM to load
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+});
